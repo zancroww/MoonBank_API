194af08e27a3cd71ee2c1133a7ba33d77aa07f7e
Evan Hosking <evanhosking@hotmail.co.uk>
2023-08-25 16:33:20 +0100

    Initial functionality added

--- db.py
+++ db.py
@@ -1,37 +1,17 @@
-#db.py
-import os
 import pymysql
-from flask import jsonify
-
-db_user = os.environ.get('CLOUD_SQL_USERNAME')
-db_password = os.environ.get('CLOUD_SQL_PASSWORD')
-db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
-db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
-
 
 def open_connection():
-    unix_socket = '/cloudsql/{}'.format(db_connection_name)
     try:
-        if os.environ.get('GAE_ENV') == 'standard':
-            conn = pymysql.connect(user=db_user, password=db_password,
-                                unix_socket=unix_socket, db=db_name,
-                                cursorclass=pymysql.cursors.DictCursor
-                                )
+        conn = pymysql.connect(
+        host='localhost',
+        user='root', 
+        password = "root", # hide password
+        db='MoonBank',
+        )
     except pymysql.MySQLError as e:
         print(e)
 
     return conn
 
-
-
-def get_bank_accounts(userrequest):
-    conn = open_connection()
-    with conn.cursor() as cursor:
-        result = cursor.execute('SELECT * FROM bank_accounts WHERE userid_fk = (userid);' VALUES(%s)', (userequest["userid"]))))
-        songs = cursor.fetchall()
-        if result > 0:
-            got_songs = jsonify(songs)
-        else:
-            got_songs = 'No Songs in DB'
+def close_connection(conn):
     conn.close()
-    return got_songs
